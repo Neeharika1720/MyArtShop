090ee2a7eac72b1c7183b5c01d7a25e111b32486
Neeharika1720 <Neeharika2290@gmail.com>
2025-01-20 10:50:38 +0100

    Initial Commit for the Project

---
+++ MyArtShop/src/main/java/com/folkart/MyArtShop/MyArtShopApplication.java
@@ -0,0 +1,13 @@
+package com.folkart.MyArtShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyArtShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyArtShopApplication.class, args);
+	}
+
+}
---
+++ MyArtShop/src/test/java/com/folkart/MyArtShop/MyArtShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.folkart.MyArtShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyArtShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
